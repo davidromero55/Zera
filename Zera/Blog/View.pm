f9d68ac70aeb0dd898ff22fad6300f23ff0a7d3d
David Romero <romdav@gmail.com>
2018-03-17 22:11:44 -0700

    Use the app title on the blog name

--- Zera/Blog/View.pm
+++ Zera/Blog/View.pm
@@ -1,14 +1,15 @@
 package Zera::Blog::View;
 
 use JSON;
+use Zera::Conf;
 use base 'Zera::Base::View';
 
 # Module Functions
 sub display_home {
     my $self = shift;
-    
-    $self->set_title('Blog');
-    
+
+    $self->set_title($conf->{App}->{Name});
+
     # Entries
     my $entries = $self->selectall_arrayref(
         "SELECT entry_id, url, title, description, display_options, DATE(e.date) AS date " .
@@ -16,29 +17,29 @@ sub display_home {
     foreach my $entry (@$entries){
         $entry->{display_options} = decode_json($entry->{display_options});
     }
-    
+
     my $vars = {
         entries => $entries,
     };
-    
+
     return $self->render_template($vars);
 }
 
 sub display_item {
     my $self = shift;
-       
+
     # Entry
     my $entry = $self->selectrow_hashref(
         "SELECT entry_id, url, title, description, keywords, display_options, DATE(e.date) AS date, content " .
         "FROM entries e WHERE url=? AND module='Blog' AND active=1 ORDER BY e.date DESC",{Slice=>{}}, $self->param('SubView'));
     $entry->{display_options} = decode_json($entry->{display_options});
-    
+
     $self->set_title($entry->{title});
-    
+
     my $vars = {
         entry => $entry,
     };
-    
+
     return $self->render_template($vars);
 }
 
