667bf6cbcc833127d2aba56beb41d7823db91d9d
Alt180 <a291516@uach.mx>
2018-05-08 12:58:37 -0600

    Installation script in Ubuntu updated to fix several errors.

--- install/install-requirements.ubuntu.sh
+++ install/install-requirements.ubuntu.sh
@@ -5,33 +5,34 @@ if [[ $PWD != */Zera/install ]]; then
 fi
 cd ..
 apt-get update -y
+apt-get install build-essential -y
 apt-get install apache2 -y
 apt-get install mysql-server -y
+apt-get install libmysqlclient-dev -y
 #apt-get install expect -y
 #apt-get install git -y
 #git --version
-apt-get install build-essential -y
 #git clone https://github.com/Alt180/Zera.git
 #cd Zera
-service start apache2
-service enable apache2
-service start mysql
-service enable mysql
+service apache2 start
+systemctl enable apache2
+service mysql start
+systemctl enable mysql
 yes | cp install/Ubuntu/000-default.conf /etc/apache2/sites-available/000-default.conf
 yes | cp install/Ubuntu/.htaccess /var/www/html/.htaccess
-service restart apache2
+a2enmod rewrite
+a2enmod cgi.load
+a2enmod dbd.load
+service apache2 restart
 cp -r * /var/www/html
 cd /var/www/html
 yes | rm -r install
-#chown -R apache:apache *
+yes | rm .gitignore
+chown -R www-data:www-data *
 chmod 755 index.pl
-cd ..
-chown -R apache:apache html
 #firewall-cmd --permanent --add-port=80/tcp
 #firewall-cmd --reload
-#apt-get remove emacs -y
-#apt-get remove expect -y
-apt-get install perl-CPAN -y
-(echo y;echo sudo;echo y; echo exit)|cpan
+#(echo y;echo sudo;echo y; echo exit)|cpan
+(echo y; echo exit)|cpan
 yes | cpan install CGI::Minimal Template JSON Switch Email::Sender::Simple MIME::Entity Math::Round Apache::Session::MySQL DBI DBD::mysql Number::Format
-reboot
+#reboot
