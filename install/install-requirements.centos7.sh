c83d67fb55de0f93ca3629f7616fa0cabf3dcb95
Alt180 <a291516@uach.mx>
2018-06-04 17:17:55 -0600

    Assigned CPANM as Perl module installer and fixed a file path bug.

--- install/install-requirements.centos7.sh
+++ install/install-requirements.centos7.sh
@@ -19,7 +19,7 @@ systemctl enable httpd.service
 systemctl start mariadb
 systemctl enable mariadb
 sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
-yes | cp /install/CentOS7/httpd.conf /etc/httpd/conf/httpd.conf
+yes | cp install/CentOS7/httpd.conf /etc/httpd/conf/httpd.conf
 cp -r * /var/www/html
 cd /var/www/html
 yes | rm -r install
@@ -28,8 +28,9 @@ chown -R apache:apache *
 chmod 755 index.pl
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --reload
-yum install perl-CPAN -y
-(echo y;echo sudo;echo y; echo exit)|cpan
-yes | cpan install CGI::Minimal Email::Sender:Simple MIME::Entity Apache::Session Number::Format JSON::XS Math::Round
+#yum install perl-CPAN -y
+#(echo y;echo sudo;echo y; echo exit)|cpan
+curl -L http://cpanmin.us | perl - --self-upgrade
+yes | cpanm install CGI::Minimal Email::Sender::Simple MIME::Entity Apache::Session Number::Format JSON::XS Math::Round
 systemctl restart httpd.service
 reboot
