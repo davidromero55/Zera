dcd17d2e67824608a5dc243e64f557c0d633ecbb
Alt180 <a291516@uach.mx>
2018-07-19 09:56:06 -0600

    Counter implemented in elements deployment.

--- Zera/Blog/Components.pm
+++ Zera/Blog/Components.pm
@@ -5,7 +5,8 @@ use JSON;
 
 sub component_latest {
     my $self = shift;
-    my $latest = $self->selectall_arrayref("SELECT SQL_CACHE entry_id, title, date, url, description, display_options FROM entries NATURAL JOIN entries_categories WHERE active=1 AND category_id=1 ORDER BY date DESC",{Slice=>{}});
+    my $limit = shift;
+    my $latest = $self->selectall_arrayref("SELECT SQL_CACHE entry_id, title, date, url, description, display_options FROM entries NATURAL JOIN entries_categories WHERE active=1 AND category_id=1 ORDER BY date DESC limit ?",{Slice=>{}}, $limit);
     foreach my $entry (@$latest){
         $entry->{display_options} = decode_json($entry->{display_options});
     }
@@ -19,7 +20,8 @@ sub component_latest {
 
 sub component_footer {
     my $self = shift;
-    my $latest = $self->selectall_arrayref("SELECT SQL_CACHE entry_id, title, date, url FROM entries NATURAL JOIN entries_categories WHERE active=1 AND category_id=1 ORDER BY date DESC",{Slice=>{}});
+    my $limit = shift;
+    my $latest = $self->selectall_arrayref("SELECT SQL_CACHE entry_id, title, date, url FROM entries NATURAL JOIN entries_categories WHERE active=1 AND category_id=1 ORDER BY date DESC limit ?",{Slice=>{}}, $limit);
     my $vars = {
         latest  => $latest,
     };
