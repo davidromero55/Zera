1972f8d8324d91f8e71bda6f2d92b99f5cff9016
Alt180 <a291516@uach.mx>
2018-09-03 11:25:34 -0600

    Duplication fixed.

--- Zera/Install/tmpl/Conf.pm
+++ Zera/Install/tmpl/Conf.pm
@@ -1,4 +1,3 @@
-<<<<<<< current
 package Zera::Conf;
 
 use strict;
@@ -63,69 +62,3 @@ $conf->{Email} = {
 };
 
 1;
-=======
-package Zera::Conf;
-
-use strict;
-BEGIN {
-    use Exporter();
-    use vars qw( @ISA @EXPORT @EXPORT_OK );
-    @ISA = qw( Exporter );
-    @EXPORT = qw(
-                $conf
-                );
-}
-use vars @EXPORT;
-
-$conf->{DBI} = {
-    Conection => 'dbi:mysql:<% Database.Database %>',
-    Username  => '<% Database.Username %>',
-    Password  => '<% Database.Password %>',
-    Charset   => 'utf8',
-    Timezone  => '<% Database.Timezone %>',
-    Database  => '<% Database.Database %>',
-    Language  => '<% Database.Language %>'
-};
-
-$conf->{App} = {
-   Name      => '<% Website.Name %>',
-   Version   => '0.1',
-   URL       => '<% Website.URL %>',
-   Language  => '<% Database.Language %>',
-   URLLink   => 'http://<% Website.URL %>',
-   Copyright => '<% Website.Name %>',
-   Language  => 'en_US',
-   TimeZone  => '<% Database.Timezone %>',
-};
-
-$conf->{Cookie} = {
-    Name      => 'ZERA',
-    'Max-Age' => '31536000',
-    Domain    => $conf->{App}->{URL},
-    SameSite  => 'Strict',
-    Path      => '/',
-};
-
-$conf->{Template} = {
-    TemplateID      => 'ZeraWebsite',
-    UserTemplateID  => 'ZeraUser',
-    AdminTemplateID => 'ZeraAdmin',
-};
-
-$conf->{Security} = {
-    Key => '<% SecurityKey %>',
-};
-
-$conf->{Email} = {
-    Server   => 'localhost',
-    Port     => '587',
-    Auth     => 'LOGIN',
-    User     => '',
-    Password => '',
-    From     => '<% User.Email %>',
-    SSL      => 'starttls',
-    Debug    => '0',
-};
-
-1;
->>>>>>> before discard
