33fc3530842bc447807c0b0c75aff0bf9d787f81
davidromero55 <romdav@gmail.com>
2018-10-09 20:31:15 -0600

    Move js files to a better place

--- Zera/Admin/js/zera_admin.js
+++ Zera/Admin/js/zera_admin.js
@@ -60,8 +60,6 @@ $(document).ready(function(){
         $('.datepicker').datepicker({
             format: 'yyyy-mm-dd'
         });
-    } else {
-      console.log("Error in admin datepicker")
     }
 
 });
