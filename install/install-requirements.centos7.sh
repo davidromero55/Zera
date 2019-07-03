a863e7448b9e1f827f5cb78356159d832e56c88f
root <root@pc.us-west1-b.c.xaandia.internal>
2019-07-03 20:22:16 +0000

    Details for centos7 install

--- install/install-requirements.centos7.sh
+++ install/install-requirements.centos7.sh
@@ -1,4 +1,3 @@
-#dhclient
 if [[ $PWD != */Zera/install ]]; then
     echo "Please, install from Zera/install directory"
     exit 1
@@ -11,9 +10,9 @@ yum groupinstall "Development tools" -y
 yum install 'perl(Template)' -y
 yum install 'perl(JSON)' -y
 yum install 'perl(Switch)' -y
-#yum install git -y
-#git clone https://github.com/Alt180/Zera.git
-#cd Zera
+yum install perl-CPAN -y
+curl -L http://cpanmin.us | perl - --self-upgrade
+yes | cpanm CGI::Minimal Email::Sender::Simple MIME::Entity Apache::Session Number::Format JSON::XS Math::Round IO::Socket::SSL MIME::Base64 Authen::SASL
 systemctl start httpd.service
 systemctl enable httpd.service
 systemctl start mariadb
@@ -23,14 +22,7 @@ yes | cp install/CentOS7/httpd.conf /etc/httpd/conf/httpd.conf
 cp -r * /var/www/html
 cd /var/www/html
 yes | rm -r install
-yes | rm -r testing
 chown -R apache:apache *
 chmod 755 index.pl
-firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --reload
-yum install perl-CPAN -y
-#(echo y;echo sudo;echo y; echo exit)|cpan
-curl -L http://cpanmin.us | perl - --self-upgrade
-yes | cpanm CGI::Minimal Email::Sender::Simple MIME::Entity Apache::Session Number::Format JSON::XS Math::Round IO::Socket::SSL MIME::Base64 Authen::SASL
 systemctl restart httpd.service
 reboot
